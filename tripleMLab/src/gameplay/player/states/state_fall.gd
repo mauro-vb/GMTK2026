fa6f2fd0a830c5bdb012c632ac7af9b4e62babcd
the_walter <msichtermann@hotmail.de>
2026-07-24 00:53:11 +0200

    added smoother movements -> better gravity, jump hang, apex movement, left right no longer cancel eachother

--- tripleMLab/src/gameplay/player/states/state_fall.gd
+++ tripleMLab/src/gameplay/player/states/state_fall.gd
@@ -8,7 +8,7 @@ func physics_update(delta: float) -> void:
 	player.apply_horizontal_movement(delta)
 
 	if player.is_on_floor():
-		transitioned.emit(PlayerState.STATE_ID.RUN if Input.get_axis("left", "right") != 0 else PlayerState.STATE_ID.IDLE)
+		transitioned.emit(PlayerState.STATE_ID.RUN if player.get_movement_direction() != 0 else PlayerState.STATE_ID.IDLE)
 		return
 
 	if player.can_jump():
