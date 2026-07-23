7cb52fde808bcc9ce05ec0ed17e920c835fb59b7
the_walter <msichtermann@hotmail.de>
2026-07-23 23:39:36 +0200

    refactored state names to enum

--- tripleMLab/src/gameplay/player/states/state_run.gd
+++ tripleMLab/src/gameplay/player/states/state_run.gd
@@ -1,16 +1,19 @@
 extends PlayerState
 class_name StateRun
 
+func get_state_id() -> PlayerState.ID:
+	return PlayerState.ID.RUN
+
 func physics_update(delta: float) -> void:
 	player.apply_horizontal_movement(delta)
 
 	if not player.is_on_floor():
-		transitioned.emit("StateFall")
+		transitioned.emit(PlayerState.ID.FALL)
 		return
 
 	if player.can_jump():
-		transitioned.emit("StateJump")
+		transitioned.emit(PlayerState.ID.JUMP)
 		return
 
 	if Input.get_axis("left", "right") == 0:
-		transitioned.emit("StateIdle")
+		transitioned.emit(PlayerState.ID.IDLE)
