866bb1dc0b888ffb966af075d9816313a2cdf6b3
mauro-vb <mvb.codes@gmail.com>
2026-07-24 15:55:05 +0200

    started time system structure

---
+++ tripleMLab/src/gameplay/systems/time_system.gd
@@ -0,0 +1,30 @@
+class_name TimeSystem
+extends Node
+
+# Signals
+signal max_time_changed
+signal time_expired
+
+# Constants
+const START_TIME: float = 60.0
+
+# Exports
+
+# Public
+# Private
+# On Ready
+
+# Static
+
+# Lifecycle
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+# Public
+
+# Private
+
+# Callbacks
