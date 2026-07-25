82a515641fb8d3d34b26cc497d85ad9ac7e1afab
mauro-vb <mvb.codes@gmail.com>
2026-07-26 00:29:00 +0200

    uid wasnt beign recognised

--- tripleMLab/src/autoloads/uids.gd
+++ tripleMLab/src/autoloads/uids.gd
@@ -9,7 +9,7 @@ const MODIFIER_DISPLAY_SCENE_UID: String = "uid://dv6ct8mkr3jqw"
 # WORLD
 const MAP_SCENE_UID: String = "uid://ipmc68r6n333"
 const PLAYER_SCENE_UID: String = "uid://kwjq37d8yab5"
-const TEST_LEVEL_UID: String = "uid://bxu2auvoeusi3"
+const TEST_LEVEL_UID: String = "res://src/levels/initial_levels/InitialLevel1.tscn"
 
 # SYSTEMS
 const TIME_SYSTEM_UID: String = "uid://bjktioxpxjuo"
